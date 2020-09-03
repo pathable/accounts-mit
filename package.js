93fb9a1a53427df7d59be1aad2196f0c0ed86004
Igor Fontana <igor193@gmail.com>
2020-09-03 12:52:50 -0300

    bumping versions

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 Package.describe({
   summary: 'Login service for Mit accounts',
-  version: '1.0.2',
+  version: '1.0.3',
   name: 'pathable:accounts-mit',
   git: 'https://github.com/pathable/accounts-mit',
 });
 
 Package.onUse(api => {
-  api.versionsFrom('1.11')
+  api.versionsFrom('1.10.3-beta.9')
 
   api.use('ecmascript');
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
 
-  api.use('pathable:accounts-oauth@1.3.3', ['client', 'server']);
-  api.use('pathable:mit-oauth@1.0.0');
+  api.use('accounts-oauth', ['client', 'server']);
+  api.use('pathable:mit-oauth@1.0.3');
   api.imply('pathable:mit-oauth');
 
   api.addFiles('mit.js');
