9360207db72f9a36988cbade38c54756fda8d43f
Igor Fontana <igor193@gmail.com>
2021-08-05 08:03:23 -0300

    TP-15511 Upgrade to Meteor 2.3 - upgrade oauth

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: 'Login service for Mit accounts',
-  version: '1.0.4',
+  version: '1.1.0',
   name: 'pathable:accounts-mit',
   git: 'https://github.com/pathable/accounts-mit',
 });
 
 Package.onUse(api => {
-  api.versionsFrom('1.10.3-beta.9');
+  api.versionsFrom('2.3.4');
 
   api.use('ecmascript');
   api.use('accounts-base', ['client', 'server']);
@@ -14,7 +14,7 @@ Package.onUse(api => {
   api.imply('accounts-base', ['client', 'server']);
 
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('pathable:mit-oauth@1.0.4');
+  api.use('pathable:mit-oauth@1.1.0');
   api.imply('pathable:mit-oauth');
 
   api.addFiles('mit.js');
